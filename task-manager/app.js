41ef106e48c94629ccd13915892b9ab04ff678f9
akshpnwr <akashpanwar9976@gmail.com>
2024-02-17 10:24:23 +0530

    Connecting to mongodb atlas cluster

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 const app = express()
 
@@ -9,6 +11,15 @@ app.use(express.json())
 // routes
 app.use('/api/v1/tasks', tasks)
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
