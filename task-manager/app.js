331744a7e961164a01e56e554bdd9ba966ebb069
akshpnwr <akashpanwar9976@gmail.com>
2024-02-20 15:53:55 +0530

    Custom not found url

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
 require('dotenv').config()
 
 const app = express()
 
 // middleware
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound);
 
 const start = async () => {
     try {
