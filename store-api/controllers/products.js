d29e4f36bc93c690fc7ac6487b8aaff37bada345
akshpnwr <akashpanwar9976@gmail.com>
2024-02-21 22:05:59 +0530

    setting up query filters

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -7,7 +7,20 @@ const populateDb = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find()
+    const { featured, name, company } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    if (company) {
+        queryObject.company = company
+    }
+
+    const products = await Product.find(queryObject)
     res.status(200).json({ nbHits: products.length, products })
 }
 
