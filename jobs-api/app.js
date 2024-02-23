f09c0b6b9f6b947b4541219784a86ebfc231e929
akshpnwr <akashpanwar9976@gmail.com>
2024-02-23 12:00:44 +0530

    Add auth and jobs routes and controllers

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -10,10 +10,13 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 // extra packages
 
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
